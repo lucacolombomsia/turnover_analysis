aa5d13114d9060429a2765d93ae35bc34c0dcc6b
Luca Colombo <lucacolombo2018@u.northwestern.edu>
2018-02-06 23:55:42 -0600

    can now read form input, next step use it for prediction

--- app/turnover_analysis_localapp.py
+++ app/turnover_analysis_localapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,10 +6,23 @@ app = Flask(__name__)
 def index():
     return 'This is the homepage'
 
-#Using HTML Templates
-@app.route("/get_inputs")
-def get_inputs():
-    return render_template("inputs.html")
+@app.route("/inputs")
+def my_form():
+    return render_template('my-form.html')
+
+@app.route('/inputs', methods=['POST'])
+def my_form_post():
+    x = [1] + [float(request.form['satisfaction'])]
+    x += [float(request.form['evaluation'])]
+    x += [float(request.form['projects'])]
+    x += [float(request.form['hours'])]
+    x += [float(request.form['tenure'])]
+    x += [request.form.get('accident')]
+    x += [request.form.get('promotion')]
+    x += [request.form.get('department')]
+    x += [request.form.get('salary')]
+    y = [str(n) for n in x]
+    return str(','.join(y))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- develop/src/make-db.py
+++ develop/src/make-db.py
@@ -2,12 +2,12 @@ import pandas as pd
 from sqlalchemy import create_engine
 import config
 
-def make_local_db() :
+def make_employees_table() :
     employees = pd.read_csv('../data/turnover.csv')
     employees.insert(0, 'empID', range(1, len(employees)+1))
-    print(employees.shape)
     engine = create_engine(config.database_config)
     employees.to_sql(name = 'employees', con = engine, if_exists='replace', index=False)
+    print("Created employees table")
 
 if __name__ == "__main__":
-    make_local_db()
+    make_employees_table()
