ff0017e360097c970b220546f9a2241333a362cb
Luca Colombo <lucacolombo2018@u.northwestern.edu>
2018-07-30 11:39:31 -0500

    preparing for internship presentation

--- develop/src/dbconfig.py
+++ develop/src/dbconfig.py
@@ -2,9 +2,9 @@ import os
 
 dbname = os.environ.get('DATABASE')
 user = os.environ.get('USERNAME')
-port = os.environ.get('PORT')
-pw = os.environ.get('PASSWORD')
 host = os.environ.get('HOST')
 
-database_config = ("postgresql://" + user + ":" + pw +
-                   "@" + host + ":" + port + "/" + dbname)
+database_config = ("postgresql://" + user + "@" + host + "/" + dbname)
+
+'''database_config = ("postgresql://" + user + ":" + pw +
+                   "@" + host + ":" + port + "/" + dbname)'''
