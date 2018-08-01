cb5de949291a1fe874622a56d050ebf18fd7654d
Luca Colombo <lucacolombo2018@u.northwestern.edu>
2018-07-31 19:36:35 -0500

    need a branch for internship pres

--- develop/src/dbconfig.py
+++ develop/src/dbconfig.py
@@ -3,8 +3,8 @@ import os
 dbname = os.environ.get('DATABASE')
 user = os.environ.get('USERNAME')
 host = os.environ.get('HOST')
+port = os.environ.get('PORT')
+pw = os.environ.get('PASSWORD')
 
-database_config = ("postgresql://" + user + "@" + host + "/" + dbname)
-
-'''database_config = ("postgresql://" + user + ":" + pw +
-                   "@" + host + ":" + port + "/" + dbname)'''
+database_config = ("postgresql://" + user + ":" + pw +
+                   "@" + host + ":" + port + "/" + dbname)
